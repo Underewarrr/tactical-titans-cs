e0ca8a215db0f6e361c9298546a2364c3616f41b
Underewarrr <rafhaelxd@gmail.com>
2023-06-15 19:36:38 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const path = require('path');
-const fs = require('fs');
-
 module.exports = {
   async headers() {
     return [
@@ -14,19 +11,5 @@ module.exports = {
         ],
       },
     ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/ads.txt',
-        destination: '/ads.txt',
-      },
-    ];
-  },
-  async afterExport() {
-    const adsFilePath = path.join(__dirname, 'ads.txt'); // Change the path if necessary
-    const staticAdsPath = path.join(__dirname, '.next', 'static', 'ads.txt'); // Change the path if necessary
-
-    fs.copyFileSync(adsFilePath, staticAdsPath);
   },
 };
